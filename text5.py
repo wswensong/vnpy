9fb1390ee752d69c056c19d23186823303bbf229
wsuong <wswensong@gmail.com>
2025-01-24 20:21:42 +0800

    更改

--- text5.py
+++ text5.py
@@ -6,25 +6,17 @@ def classify_items(items_to_classify):
     # 遍历要分类的商品，并将其归入合适的类别
     for item in items_to_classify:
         found = False
-        for main_category, sub_categories in categories.items():
-            if isinstance(sub_categories, dict):
-                for sub_category, items in sub_categories.items():
-                    if item in items:
-                        if main_category not in classified_items:
-                            classified_items[main_category] = {}
-                        if sub_category not in classified_items[main_category]:
-                            classified_items[main_category][sub_category] = []
-                        classified_items[main_category][sub_category].append(item)
-                        found = True
-                        break
-            else:
-                if item in sub_categories:
-                    if main_category not in classified_items:
-                        classified_items[main_category] = []
-                    classified_items[main_category].append(item)
-                    found = True
-            if found:
+        for category, items in categories.items():
+            if item in items:
+                if category not in classified_items:
+                    classified_items[category] = []
+                classified_items[category].append(item)
+                found = True
                 break
+        if not found:
+            if "其他" not in classified_items:
+                classified_items["其他"] = []
+            classified_items["其他"].append(item)
 
     return classified_items
 
